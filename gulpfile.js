8ca1855e8831508eb6240ff24ffa7edeab986004
mchaplynskikh <foleycpk@gmail.com>
2025-04-12 16:02:33 +0300

    test commit

--- gulpfile.js
+++ gulpfile.js
@@ -26,20 +26,22 @@ function clean() {
   return del('dist');
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
-
-
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/blocks/**/*.css'], css);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
 }
 
+const build = gulp.series(clean, gulp.parallel(html, css, images));
 const watchapp = gulp.parallel(build, watchFiles);
+
+
+
 
 exports.html = html; 
 exports.css = css; 
 exports.images = images; 
 exports.clean = clean; 
+
 exports.build = build; 
 exports.watchapp = watchapp; 
